d07602ee9dc2b82718ac3b8bbadd45465aa5d65e
Momen <mark.morcos@drive-finance.com>
2025-08-23 14:21:53 +0300

    update page modal to get pages from sql db

--- src/database/types/quranPageData.ts
+++ src/database/types/quranPageData.ts
@@ -5,3 +5,11 @@ export type QuranPageData = {
   lines: { line: QuranLineType; words: QuranWordType[] }[];
   surah: string;
 };
+
+// page_id	page_number	juz_id	surah_name
+export type QuranMenuPageType = {
+  page_id: number;
+  page_number: number;
+  juz_id: number;
+  surah_name: string;
+};
