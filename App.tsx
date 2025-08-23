a79292b61eb0d9d429703c4affb20423de5be767
Momen <mark.morcos@drive-finance.com>
2025-08-23 14:20:41 +0300

    add safe area provider

--- App.tsx
+++ App.tsx
@@ -5,23 +5,26 @@
  * @format
  */
 
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import AppContainer from './src/navigation';
 import Toast from 'react-native-toast-message';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Provider store={store}>
-        <AppContainer />
-      </Provider>
-      <Toast />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Provider store={store}>
+          <AppContainer />
+        </Provider>
+        <Toast />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
